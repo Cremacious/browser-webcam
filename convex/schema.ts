41d6f163309476678c7fcef6c98ca64ac56458c8
cremacious <chrismackall3@gmail.com>
2025-06-13 12:00:14 -0400

    Svix installed with new webhook

--- convex/schema.ts
+++ convex/schema.ts
@@ -10,3 +10,6 @@ export default defineSchema({
     clerkId: v.string(),
   }).index('by_clerk_id', ['clerkId']),
 });
+
+
+// https://upward-reptile-77.clerk.accounts.dev
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
+
 export default function Home() {
-  return <>Home</>;
+  return (
+    <>
+      Home
+      <div>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
+    </>
+  );
 }
