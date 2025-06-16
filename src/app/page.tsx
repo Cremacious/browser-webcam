31500ab5596583cb1d567934c1be2aa715a02bcb
cremacious <chrismackall3@gmail.com>
2025-06-16 12:38:44 -0400

    New schemas and setup for navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,4 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
 
 export default function Home() {
-  return (
-    <>
-      Home
-      <div>
-        <SignedOut>
-          <SignInButton />
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-      </div>
-    </>
-  );
+  return <>Home</>;
 }
